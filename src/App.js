a220d1145ce12660c26b78a49208765bda3bc6c2
Fahrad <levonyanfahrad20@gmail.com>
2024-06-11 11:42:35 +0400

    test spa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { Navbar } from './Navbar';
+import { Route, Routes } from 'react-router';
+import { Layout } from './Layout';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import { Portfolio } from './pages/Portfolio';
 
 function App() {
   return (
-    <>
-      <Navbar />
-    </>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+      </Routes>
+    </Layout>
   );
 }
 
